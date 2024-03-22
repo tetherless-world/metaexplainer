521d8bb7f394a76f5905721dee5a56ce495ef736
Shruthi Chari <charishruthi@gmail.com>
2024-03-21 21:47:34 -0400

    Printing sample questions

--- metaexplainer/metaexplainercode/decompose/read_questions.py
+++ metaexplainer/metaexplainercode/decompose/read_questions.py
@@ -1,5 +1,6 @@
 import re
 import nltk
+import random
 
 import sys
 sys.path.append('../')
@@ -14,6 +15,17 @@ def read_utterances_file(utterances_path):
 		return questions_operations
 	return []
 
+def print_sample(utterances_pairs):
+	'''
+	Randomly print top-5 questions from utterance pairs
+	'''
+	rands = random.sample(range(0, len(utterances_pairs)), 5)
+
+	for i in rands:
+		print('Question ', utterances_pairs[i]['question'])
+		print('Operations ', utterances_pairs[i]['talk_to_model_operations'])
+		print('-----')
+
 if __name__=='__main__':
 	german_questions = read_utterances_file('german_test_suite.txt')
 	diabetes_questions = read_utterances_file('diabetes_test_suite.txt')
@@ -23,3 +35,16 @@ if __name__=='__main__':
 		' Diabetes ', len(diabetes_questions), '\n',
 		' COMPAS ', len(compas_questions), '\n',
 		' German credit ', len(compas_questions))
+
+	print('Diabetes samples')
+	print_sample(diabetes_questions)
+	print('*****'*5)
+
+	print('Compas samples ')
+	print_sample(compas_questions)
+	print('*****'*5)
+
+	print('German credit samples')
+	print_sample(german_questions)
+	print('*****'*5)
+
