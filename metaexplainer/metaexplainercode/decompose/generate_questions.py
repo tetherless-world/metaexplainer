14c20d8f3587cefefa37149f3d7825713a429f9d
Shruthi Chari <charishruthi@gmail.com>
2024-04-13 12:10:16 -0400

    Getting model to use Jinja templates and run across explanation types using question types - now need to nail the hallucinations in terms of the machine interpretations

--- metaexplainer/metaexplainercode/decompose/generate_questions.py
+++ metaexplainer/metaexplainercode/decompose/generate_questions.py
@@ -1,36 +1,89 @@
 from openai import OpenAI
 client = OpenAI()
 
+from jinja2 import Template
+import pandas as pd
+
 import sys
 sys.path.append('../')
 from metaexplainercode import codeconstants
 
+def retrieve_prompt():
+	#certain kind of prediction and features in certain ways, and that fact that it uses confidence 
+	prompt_template_text = '''
+ Generate 100 questions for {{explanation}} for the Diabetes domain. {{explanation}} questions are of the form, 
+ {%- for question in questions %}
+	{{question}} \n
+	{%- endfor %}
+
+
+	Your features could be:
+	 {%- for feature in features %}
+	{{feature}} \n
+	{%- endfor %}.
+	 and the prediction is {{prediction}}. 
+
+	 The feature values range from 
+	 {%- for feature_range in feature_ranges %}
+	{{feature_range}} \n
+	{%- endfor %}.
+
+	For each question, also generate a machine interpretation which includes a predicate logic interpretation of the question, the explanation type, features and also identify the target variable and if there is a low / high likelihood.
+	'''
+
+	return prompt_template_text
+
+
+def create_prompt_record(explanation_type, questions, features, prediction, feature_ranges):
+	#print('Trial ', sample)
+	return {
+		"explanation" : explanation_type,
+		"questions" : questions,
+		"features" : features,
+		"prediction": prediction,
+		"feature_ranges" : feature_ranges
+	}
+
 if __name__=="__main__":
 
-  conts = ''
+	conts = ''
+	prompt_template_text = retrieve_prompt()
+	print(prompt_template_text)
+	prompt_template = Template(prompt_template_text)
 
-  for i in range(0, 3):
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      # {"role": "system",
-      #  "content": "You are an explanation assistant skilled at understanding the needs of users and the questions they could ask for which they require explanations. Anticipate what their needs might be based on the user's requirements and generate questions."},
-       {"role": "system",
-       "content": "Always answer in English."},
-      {"role": "user",
-       "content": "Generate 100 questions for contrastive explanations for the Diabetes domain. Contrastive questions are of the form, 'Why this and not that' or 'Why this feature versus this feature'. " \
-       "Your features could be age, diabetes pedigree function, sex and BMI and the prediction is whether the patient has diabetes or not. Their values range from ages: 18 - 70, diabetes pedigree function: 0.2 - 1, sex: Male or female and BMI: 18 - 30." \
-       " For each question, also generate a machine interpretation which includes a predicate logic interpretation of the question, features and also identify the target variable and if there is a low / high likelihood."}
-       #recognizing in the question - all instances, all features - their filters and the values that are being compared, all datasets and the target variable and whether it is a low or high likelihood.
-    ],
-    temperature=0
-    #above part of prompt 2 can be used across question types
-  )
+	#define the features and feature ranges on a per dataset basis - can have a common util function for this
+	features = ['Age', 'Sex', 'Diabetes Pedigree Function', 'BMI']
+	feature_ranges = ['Age: 30 - 70', 'Sex: Male or Female', 'Diabetes Pedigree Function: 0.2 - 0.1', 'BMI: 18 - 30']
 
-    output = completion.choices[0].message
-    conts += output.content + '\n \n'
+	selected_explanation_types = ['Case Based Explanation', 'Contrastive Explanation', 'Data Explanation', 'Rationale Explanation', 'Contextual Explanation', 'Counterfactual Explanation']
+	questions_file = pd.read_csv(codeconstants.OUTPUT_FOLDER + '/prototypical_questions_explanations_eo.csv')
 
-    print("Run ", str(i))
+	for explanation_type in selected_explanation_types:
+		questions_explanation = (questions_file[questions_file['explanation'] == explanation_type]['question']).to_list()
+		prompt = create_prompt_record(explanation_type, questions_explanation, features, 'whether a patient has Diabetes or not',feature_ranges)
+		filled_prompt = prompt_template.render(prompt)
+		print(filled_prompt)
 
-  with open(codeconstants.OUTPUT_FOLDER + '/gpt-questions.txt', 'w') as f:
-    f.write(conts)
+		for i in range(0, 1):
+			completion = client.chat.completions.create(
+				model="gpt-3.5-turbo",
+				messages=[
+					# {"role": "system",
+					#  "content": "You are an explanation assistant skilled at understanding the needs of users and the questions they could ask for which they require explanations. Anticipate what their needs might be based on the user's requirements and generate questions."},
+					 {"role": "system",
+					 "content": "Always answer in English."},
+					{"role": "user",
+					 "content": filled_prompt}
+					 #recognizing in the question - all instances, all features - their filters and the values that are being compared, all datasets and the target variable and whether it is a low or high likelihood.
+				],
+				temperature=0
+				#above part of prompt 2 can be used across question types
+			)
+
+			output = completion.choices[0].message
+			conts += output.content + '\n \n'
+
+			print("Run ", str(i), 'for explanation ', explanation_type)
+
+	with open(codeconstants.OUTPUT_FOLDER + '/gpt-questions.txt', 'w') as f:
+		f.write(conts)
