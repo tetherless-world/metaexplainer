1494e75635aa1acc871e27afa2b239694c763056
Shruthi Chari <charishruthi@gmail.com>
2024-05-19 13:09:34 -0400

    Able to run SPARQL on Ontospy data expalanation graph - next need to run restriction query to get the method - can use website

--- metaexplainer/metaexplainercode/delegate/extract_explainer_methods.py
+++ metaexplainer/metaexplainercode/delegate/extract_explainer_methods.py
@@ -11,6 +11,25 @@ sys.path.append('../')
 from metaexplainercode import codeconstants
 
 from metaexplainercode import metaexplainer_utils
+from metaexplainercode import ontology_utils
+
+def create_explanation_type_graph():
+	#base it off of" https://stackoverflow.com/questions/16829351/is-there-a-hello-world-example-for-sparql-with-rdflib
+	pass
+
 
 if __name__ == '__main__':
+	eo_model = ontology_utils.load_eo()
+
+	data_explanation = ontology_utils.get_class_term(eo_model, 'Data Explanation', -1)
+
+	print(dir(data_explanation))
+	#need to call buildGraph here 
+	data_explanation_graph = data_explanation.rdflib_graph
 	
+	sample_query = "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#> " \
+"prefix owl:<http://www.w3.org/2002/07/owl#> " \
+"prefix ep: <http://linkedu.eu/dedalo/explanationPattern.owl#> " \
+"prefix prov: <http://www.w3.org/ns/prov#> select ?label ?exp where { ?exp a owl:Class . ?exp rdfs:label ?label . } "
+	x = data_explanation_graph.query(sample_query)
+	print(list(x))
