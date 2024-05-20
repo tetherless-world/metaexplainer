5ab4638514f87244091a6cde60b89ecf5fd8136e
Shruthi Chari <charishruthi@gmail.com>
2024-05-19 22:36:37 -0400

    Adding start at RAG

---
+++ metaexplainer/metaexplainercode/synthesis/run_rag.py
@@ -0,0 +1,5 @@
+'''
+Run RAG on outputs from explainer methods with prompts for identified explanation type from EO
+Base it off of: https://towardsai.net/p/machine-learning/query-your-dataframes-with-powerful-large-language-models-using-langchain
+Or https://docs.llamaindex.ai/en/stable/examples/query_engine/pandas_query_engine/
+'''
