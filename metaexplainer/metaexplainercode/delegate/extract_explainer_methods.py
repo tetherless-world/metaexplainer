95071651077d8dde3efc60776bbdc0fa633dd01f
Shruthi Chari <charishruthi@gmail.com>
2024-05-22 15:46:50 -0400

    Writing all functions and explanation methods to files

--- metaexplainer/metaexplainercode/delegate/extract_explainer_methods.py
+++ metaexplainer/metaexplainercode/delegate/extract_explainer_methods.py
@@ -6,6 +6,8 @@ from rdflib import Graph
 import rdflib
 import ontospy
 
+import pandas as pd
+
 import sys
 sys.path.append('../')
 from metaexplainercode import codeconstants
@@ -37,16 +39,19 @@ def run_query_on_explanation_graph(explanation_type_label, ont_model):
 	explanation_graph = explanation.rdflib_graph
 	method_results = explanation_graph.query(explanation_method_query)
 	#print('Debugging ', list(method_results))
-	methods_instances = {}
+	methods_instances = []
 	
 	for result in method_results:
 		class_label = ontology_utils.get_label_from_URI(result[0])
-		methods_instances[class_label] = []
+		
 		#print(class_label)
 		instances_m = ontology_utils.get_instances_of_class(ont_model, class_label)
 
 		if len(instances_m) > 0:
-			methods_instances[class_label] += [ontology_utils.get_label_from_URI(instance[0], split=False) for instance in instances_m]
+			all_instances = [ontology_utils.get_label_from_URI(instance[0], split=False) for instance in instances_m]
+
+			for instance in all_instances:
+				methods_instances.append({'Explanation Type': explanation_type_label,'Methods': class_label, 'Instances': instance})
 
 	return methods_instances
 
@@ -55,9 +60,13 @@ if __name__ == '__main__':
 	eo_model = ontology_utils.load_eo()
 
 	loaded_explanations = metaexplainer_utils.load_selected_explanation_types()
+	ep_list = []
 
 	for explanation in loaded_explanations:
 		print('Explanation ', explanation)
 		explanation_methods_instances = run_query_on_explanation_graph(explanation, eo_model)
+		ep_list += explanation_methods_instances
 		print('Methods - Instances', explanation_methods_instances)
 		print('------')
+	
+	pd.DataFrame(ep_list).to_csv(codeconstants.DELEGATE_FOLDER + '/explanation_type_methods.csv')
