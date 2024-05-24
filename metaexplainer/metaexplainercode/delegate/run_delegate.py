3aa61c9fcdf1e25ed633e390b73f22dbff8a56ef
Shruthi Chari <charishruthi@gmail.com>
2024-05-24 15:54:31 -0400

    A start at a main file for delegate

---
+++ metaexplainer/metaexplainercode/delegate/run_delegate.py
@@ -0,0 +1,15 @@
+'''
+Main entry-point for delegate; so given a machine interpretation; the steps are:
+- Call parse machine interpretation (parse_machine_interpretation)
+- Get the corresponding explainer method for explanation type (extract_explainer_methods)
+- Run the explainer method (run_explainers)
+'''
+
+def retrieve_sample_decompose_passes(mode='fine-tuned'):
+    '''
+    Read from delegate output folder, if not running the method in real-time
+    '''
+    pass
+
+if __name__=='__main__':
+    pass
