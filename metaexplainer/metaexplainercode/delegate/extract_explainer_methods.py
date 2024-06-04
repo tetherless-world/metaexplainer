307dd7a03c54c48c82f6aa100be7acb73c81c69d
Shruthi Chari <charishruthi@gmail.com>
2024-06-04 18:13:37 -0400

    Adding outputs for delegate

--- metaexplainer/metaexplainercode/delegate/extract_explainer_methods.py
+++ metaexplainer/metaexplainercode/delegate/extract_explainer_methods.py
@@ -17,6 +17,25 @@ from jinja2 import Template
 from metaexplainercode import metaexplainer_utils
 from metaexplainercode import ontology_utils
 
+def run_modality_query_on_method_graph(explainer_method_label, ont_model):
+	explainer_method = ontology_utils.get_class_term(ont_model, explainer_method_label, -1)
+	explainer_graph = explainer_method.rdflib_graph
+	modality_query = Template(open(codeconstants.QUERIES_FOLDER + '/fetch_explanation_modality_explainer.html', 'r').read()).render(explainer_method_label=explainer_method_label)
+	#print('Debug',modality_query)
+	modality_results = explainer_graph.query(modality_query)
+	modality_edited = []
+	
+	for modality in modality_results:
+		modality_label = ontology_utils.get_label_from_URI(modality[0])
+		print(modality_label)
+		modality_term = ontology_utils.get_class_term(ont_model, modality_label, -1)
+
+		if len([parent for parent in modality_term.parents() if 'ExplanationModality' in str(parent)]) > 0:
+			modality_edited.append(modality_label)
+
+	print('Debugging ', list(modality_edited))
+	return modality_edited
+
 def run_query_on_explanation_graph(explanation_type_label, ont_model):
 	explanation = ontology_utils.get_class_term(ont_model, explanation_type_label, -1)
 	#since you aready have class term you don't need to pass the label
@@ -32,6 +51,11 @@ def run_query_on_explanation_graph(explanation_type_label, ont_model):
 	
 	for result in method_results:
 		class_label = ontology_utils.get_label_from_URI(result[0])
+		modalities = run_modality_query_on_method_graph(class_label, ont_model)
+		modality = ''
+
+		if len(modalities) > 0:
+			modality = modalities[0]
 		
 		#print(class_label)
 		instances_m = ontology_utils.get_instances_of_class(ont_model, class_label)
@@ -40,7 +64,7 @@ def run_query_on_explanation_graph(explanation_type_label, ont_model):
 			all_instances = [ontology_utils.get_label_from_URI(instance[0], split=False) for instance in instances_m]
 
 			for instance in all_instances:
-				methods_instances.append({'Explanation Type': explanation_type_label,'Methods': class_label, 'Instances': instance})
+				methods_instances.append({'Explanation Type': explanation_type_label,'Methods': class_label,'Modality': modality, 'Instances': instance})
 
 	return methods_instances
 
--- metaexplainer/metaexplainercode/delegate/run_delegate.py
+++ metaexplainer/metaexplainercode/delegate/run_delegate.py
@@ -9,9 +9,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-import pickle
 
-import joblib
 import time 
 
 import sys
@@ -19,72 +17,18 @@ sys.path.append('../')
 from metaexplainercode import codeconstants
 
 from metaexplainercode import metaexplainer_utils
+
+from metaexplainercode import ontology_utils
+
 from metaexplainercode.delegate.train_models.run_model_tabular import *
 
 from metaexplainercode.delegate.run_explainers import filter_records
+from metaexplainercode.delegate.extract_explainer_methods import run_modality_query_on_method_graph
 from metaexplainercode.delegate.run_explainers import TabularExplainers
 
 import random
 
-def get_domain_model(domain_name):
-	'''
-	Train model if not present 
-	'''
-	domain_dataset = metaexplainer_utils.load_dataset(domain_name)
-	
-	model_save_path = codeconstants.DELEGATE_SAVED_MODELS_FOLDER + domain_name + '/model.pkl'
 
-	if not os.path.isfile(model_save_path):
-		(X, Y) = generate_X_Y(domain_dataset, 'Outcome')
-		x_train, x_test, y_train, y_test = generate_train_test_split(domain_dataset, 'Outcome', 0.30)
-
-		transformations = transform_data(domain_dataset, [], ['Outcome'])
-
-		models = get_models()
-
-		
-		evaluate_model(models, transformations, x_train, y_train)
-		model_output = {}
-
-		for mod_num in models.keys():
-			model = models[mod_num]
-
-			(model, mod_classification_report) = fit_and_predict_model(mod_num, transformations, model, x_train, y_train, x_test, y_test)
-
-			#this is where you get the model 
-
-			model_output[mod_num] = (model, mod_classification_report)
-
-		best_model = get_best_model(model_output, 0)
-		
-		print('Stats on test dataset for best model ', best_model[0])	
-
-		#getting best output here - objective is to retrain
-		#print(model_output_print[1])
-		(model_to_save, mod_classification_report_save) = fit_and_predict_model(mod_num, transformations, best_model, X, Y, x_test, y_test, save_model=True)
-		print('Stats on entire dataset for the same best model ',mod_classification_report_save)
-		best_model_name = mod_classification_report_save['model']
-
-		#create model folders and for domain
-		metaexplainer_utils.create_folder(codeconstants.DELEGATE_SAVED_MODELS_FOLDER)
-		metaexplainer_utils.create_folder(codeconstants.DELEGATE_SAVED_MODELS_FOLDER + domain_name)
-
-		
-		transform_save_path = codeconstants.DELEGATE_SAVED_MODELS_FOLDER + domain_name + '/transformations.pkl'
-		results = mod_classification_report_save
-		
-		joblib.dump(model_to_save, model_save_path)
-		pickle.dump(mod_classification_report_save, open(codeconstants.DELEGATE_SAVED_MODELS_FOLDER + domain_name + '/results.pkl', "wb"))
-		joblib.dump(transformations, transform_save_path)
-		print('Saved model', best_model_name,' and transformations.')
-	else:
-		model_to_save = joblib.load(model_save_path)
-		transformations = joblib.load(codeconstants.DELEGATE_SAVED_MODELS_FOLDER + domain_name + '/transformations.pkl')
-		results = pickle.load(open(codeconstants.DELEGATE_SAVED_MODELS_FOLDER + domain_name + '/results.pkl', 'rb'))
-		print('Retrieved model and results.')
-
-
-	return (model_to_save, transformations, results)
 
 def load_parses(mode='fine-tuned'):
 	parse_file = codeconstants.DELEGATE_FOLDER + domain_name + '_parsed_' + mode + '_delegate_instructions.txt'
@@ -170,15 +114,18 @@ def save_results(sample_record, feature_subsets, results, explainer, explanation
 	else:
 		print('Skipped for ', explanation_type)
 
-def evaluate_explainers(explainer_objs):
+def evaluate_explainers(explainer_method, explainer_objs, eo_model):
 	'''
 	Evaluate ouput of explainers for faithfulness and monontonocity: 
 	'''
-	pass
+	modality_explainer = run_modality_query_on_method_graph(explainer_method, eo_model)
+	print(modality_explainer)
 
 if __name__=='__main__':
 	domain_name = 'Diabetes'
 	domain_dataset = metaexplainer_utils.load_dataset(domain_name)
+	eo_model = ontology_utils.load_eo()
+
 	(training_model, transformations, method_results) = get_domain_model(domain_name)
 	mode = 'generated'
 
@@ -197,4 +144,6 @@ if __name__=='__main__':
 	(method_results, explainer_objs) = run_explainer(domain_dataset, model_details, subsets, action_list, explainer_method)
 
 	save_results(sample_record, subsets, method_results, explainer_method, explanation_type)
+
+	evaluate_explainers(explainer_method, explainer_objs)
 
