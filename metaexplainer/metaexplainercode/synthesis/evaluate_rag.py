92af35bc591fcf8f47076298b586eb0a103eb573
Shruthi Chari <charishruthi@gmail.com>
2024-06-15 08:33:09 -0400

    An issue that evaluate is broken

--- metaexplainer/metaexplainercode/synthesis/evaluate_rag.py
+++ metaexplainer/metaexplainercode/synthesis/evaluate_rag.py
@@ -1,17 +1,103 @@
 from ragas import evaluate
+from ragas import evaluate
+
+from langchain.text_splitter import CharacterTextSplitter
 
 from ragas.metrics import (
     faithfulness,
     answer_relevancy,
-    context_recall,
-    context_precision,
+   context_utilization
 )
+
+
+import sys
+sys.path.append('../')
+
+from metaexplainercode import metaexplainer_utils
+from metaexplainercode import codeconstants
+
+import pandas as pd
+from datasets import Dataset
 
 '''
 Construct the question, contexts and answer fields for each result frame so that they can be evaluated.
 '''
 
-def construct_eval_record(explanations, question, subsets, result_set):
+def chunk_contexts(context): 
+    text_splitter = CharacterTextSplitter(
+        separator = "\n\n",
+        chunk_size = 20,
+        chunk_overlap  = 20
+    )
+    docs = text_splitter.create_documents([context])
+    docs = [doc.page_content for doc in docs]
+    print(len(docs))
+    return docs
+
+def construct_eval_record(dir_path, sub_dirs):
     '''
     read a result folder and generate the record 
     '''
+    explanations_res = pd.read_csv(dir_path + '/Explanations.csv')
+
+    record = pd.read_csv(dir_path + '/Record.csv')
+    question = record['Question'].item()
+
+    questions = []
+    explanations = []
+    contexts = []
+    
+    ctr = 0
+
+    for sub_dir in sub_dirs:
+        explan_record = explanations_res.iloc[ctr]
+
+        explanations.append(explan_record['Subset'])
+        subset_txt = str(metaexplainer_utils.drop_unnamed_cols(pd.read_csv(sub_dir + '/Subsets.csv')).to_string(index=False))
+        contexts.append(chunk_contexts(subset_txt))
+        questions.append(question)
+
+        explanations.append(explan_record['Explanation'])
+        explanation_txt = str(metaexplainer_utils.drop_unnamed_cols(pd.read_csv(sub_dir + '/Results.csv')).to_string(index=False))
+        contexts.append(chunk_contexts(explanation_txt))
+
+        questions.append(question)
+
+        ctr+=1
+
+    per_record_result_df = {}
+    per_record_result_df['answer'] = explanations
+    per_record_result_df['question'] = questions
+    per_record_result_df['contexts'] = contexts
+
+    #print(per_record_result_df['answers'])
+
+    return pd.DataFrame(per_record_result_df)
+
+def eval_metrics(eval_dataset):
+    result = evaluate(
+    dataset = eval_dataset, 
+    metrics=[
+        faithfulness,
+        answer_relevancy,
+    ],
+    )
+
+    df = result.to_pandas()
+    return df
+
+if __name__ == '__main__':
+    synthesis_dirs = metaexplainer_utils.read_delegate_explainer_outputs(stage='synthesis')
+    result_datasets = []
+
+    for result_dir in synthesis_dirs.keys():
+        eval_dataset = construct_eval_record(result_dir, synthesis_dirs[result_dir])
+        result_datasets.append(eval_dataset)
+    
+    result_df = pd.concat(result_datasets)
+    #result_df['contexts'] = result_df['contexts'].astype(str)
+
+    result_datasets = Dataset.from_pandas(result_df)
+    print(result_df.head(10))
+    eval_metrics = eval_metrics(result_datasets)
+    print(eval_metrics.head(10))
