7fd4c2aae78e3d0a8d870fe1592acea93180d163
Shruthi Chari <charishruthi@gmail.com>
2024-08-07 11:05:59 -0400

    Writing results - need to see how they look

--- metaexplainer/metaexplainercode/synthesis/generate_explanation_files.py
+++ metaexplainer/metaexplainercode/synthesis/generate_explanation_files.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('../')
 
 import pandas as pd
+import time
 
 from metaexplainercode import metaexplainer_utils
 from metaexplainercode import codeconstants
@@ -18,6 +19,7 @@ if __name__=='__main__':
     print(len(read_folders))
 
     rand_questions_15 = metaexplainer_utils.get_random_samples_in_list(list(read_folders.keys()), 15)
+    eval_records = []
 
     for folder in rand_questions_15:
         #need question, explanation, explanation type, metrics
@@ -25,23 +27,31 @@ if __name__=='__main__':
         quest_details = pd.read_csv(folder + '/Record.csv')
         record_folder['Question'] = quest_details['Question'][0]
         record_folder['Predicted Explanation Type'] = quest_details['Explanation type'][0]
+        record_folder['Feature groups in Question'] = quest_details['Feature groups']
 
         #there can be multiple rows in explanations - what do you do here? Show each differently or as one?
         explanation_details = pd.read_csv(folder + '/Explanations.csv')
         record_folder['Explanation of Matched Subset'] = ''
         record_folder['Explanation of Explainer Outputs'] = ''
 
+        ctr = 0
+
         for index, row in explanation_details.iterrows():
-            record_folder['Explanation of Matched Subset'] += explanation_details['Subset']
-            record_folder['Explanation of Explainer Outputs'] += explanation_details['Explanation']
+            ctr += 1
+            record_folder['Explanation of Matched Subset'] += str(ctr) + ').' + row['Subset']
+            record_folder['Explanation of Explainer Outputs'] += str(ctr) + ').' + row['Explanation']
         
         sub_folders = metaexplainer_utils.get_subfolders_in_folder(folder)
         metrics = []
 
         for sub_folder in sub_folders:
             metrics.append(pd.read_csv(sub_folder + '/Metrics.csv'))
+            
         
-        record_folder['Metrics'] = metrics
-        print(record_folder)
-        break
+        record_folder['Metrics'] = metaexplainer_utils.drop_unnamed_cols(pd.concat(metrics)).groupby(['Metric'], as_index=False).mean()
+        eval_records.append(record_folder)
         #average along the metrics 
+    
+    print('Generated eval file for ', len(rand_questions_15), 'randomly choosen results.')
+    metaexplainer_utils.create_folder(codeconstants.SYNTHESIS_FOLDER + '/eval_files/')
+    pd.DataFrame(eval_records).to_excel(codeconstants.SYNTHESIS_FOLDER + '/eval_files/evaluation_set_' + str(int(time.time())) + '.xlsx')
